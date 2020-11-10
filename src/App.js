04a87b1cd140b542cc5f433a772cb74cf9a8c362
Raian-Siddique <raian0197@gmail.com>
2020-11-10 10:01:33 +0600

    component creation finished , next pass object  to the component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const nayoks = ["Rubel", "Jalil", "Alomgir"];
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello React</h1>
-        <Person></Person>
+        <Person nayok={nayoks[0]} fetna="Moushumi"></Person>
+        <Person nayok={nayoks[1]} fetna="Shabnur"></Person>
+        <Person nayok="Manna" fetna="Purnima"></Person>
       </header>
     </div>
   );
 }
-function Person() {
-  return <h2>Habib vai is a function </h2>;
+function Person(props) {
+  var girgity = {
+    border: "3px solid cyan",
+    padding: "5px",
+    margin: "10px",
+  };
+  return (
+    // <div style={{ border: "3px solid salmon", padding: "5px", margin: "5px" }}>
+    <div style={girgity}>
+      <h1>Name:{props.nayok}</h1>
+      <h2>partnar shoytan of:{props.fetna}</h2>
+      <h3> Army of the shaytan</h3>
+    </div>
+  );
 }
 export default App;
